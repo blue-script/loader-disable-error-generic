fb7c71f3e2d60005cbae236be59536147ae187fc
blue-script <alexm92@inbox.ru>
2024-03-05 17:38:23 +0500

    3.2 Обработка ошибок Решение fix syntax

--- src/features/decks/decks-thunks.ts
+++ src/features/decks/decks-thunks.ts
@@ -32,27 +32,21 @@ export const deleteDeckTC = (id: string) => async (dispatch: Dispatch) => {
 
 export const updateDeckTC = (params: UpdateDeckParams) => async (dispatch: Dispatch) => {
   try {
-    throw new Error('Boom!!!') // for test case-3
+    // throw new Error('Boom!!!') // for test case-3
     const res = await decksAPI.updateDeck(params)
     dispatch(updateDeckAC(res.data))
   } catch (e) {
     let errorMessage: string
     if (isAxiosError<ServerError>(e)) {
-      console.log('ax')
       errorMessage = e.response ? e.response.data.errorMessages[0].message : e.message
     } else {
       errorMessage = (e as Error).message
     }
 
-    handleError(dispatch,{ message: errorMessage } )
+    handleError(dispatch, { message: errorMessage })
   }
 }
 
 type ServerError = {
-  errorMessages: [
-    {
-      field: string,
-      message: string
-    }
-  ]
+  errorMessages: Array<{ field: string, message: string }>
 }
